2251bd40999418bebd245592922d3b995ed28069
Natalie Nguyen <natalienguyen@Natalies-MacBook-Pro.local>
2019-06-12 14:38:42 -0700

    upload code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    const movies = [
+      {id: 0, 
+      title: "Godzilla: King Of The Monsters PLEASE ALLOW APPROXIMATELY 20 EXTRA MINUTES FOR PRE-SHOW AND TRAILERS BEFORE THE SHOW STARTS.",
+      overview: "Following the global success of GODZILLA and KONG: SKULL ISLAND comes the next chapter in Warner Bros. Pictures' and Legendary Pictures' cinematic MonsterVerse, an epic action adventure that pits Godzilla against some of the most popular monsters in pop culture history. "
+    },
+    {id: 1, 
+      title: "Rocketman",
+      overview: "ROCKETMAN is an epic musical fantasy about the uncensored human story of Sir Elton John as breakthrough years, starring Taron Egerton, Jamie Bell, Richard Madden and Bryce Dallas Howard."
+    },
+    {id: 0, 
+      title: "The Dead Don't Die",
+      overview: "THE DEAD DON'T DIE - the greatest zombie cast ever disassembled starring Bill Murray"
+    }
+    ];
+    let movieRows = [];
+    movies.forEach((movie)=>{
+      movieRows.push(<p key={movie.id}>{movie.title}</p>)
+    });
 
-export default App;
+    this.state ={
+      rows: movieRows
+    }
+  }
+  render (){
+    return(
+
+    <div className="container">
+      <div className="App-header">
+        <div className="logo">
+          <img src="../images/logo.png"  alt="Nata Moive"/>
+        </div>
+        <div className="right-header">
+        <h1>Movies DB Search</h1></div>
+      </div>
+      <form>
+        <div className="form-group mx-sm-3">
+          <input className="form-control" placeholder="Enter search term" type="text"/>
+          <button type="submit" className="btn btn-primary mt-2">Search</button>
+        </div>
+      </form>
+      <div className="list-group">
+       {this.state.rows}
+      </div>
+    </div>
+    )
+  }
+}
---
+++ src/components/MovieListRow.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+export default class MovieListRow extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return {
+
+        }
+    }
+}
